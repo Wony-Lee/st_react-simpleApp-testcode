4996e423bb5f3960ba5e4e6c1588ecdd25419b69
Wony-Lee <a1_11_11@naver.com>
2022-02-08 23:49:56 +0900

    api call testcode

--- src/pages/OrderPage/Type.js
+++ src/pages/OrderPage/Type.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Products from './Products'
+import { ErrorBanner } from '../../components/ErrorBanner';
 
 const Type = ({ orderType }) => {
     const [items, setItems] = useState([])
+    const [error, setError] = useState(false)
     useEffect(() => {
         loadItems(orderType)
     }, [])
@@ -13,7 +15,11 @@ const Type = ({ orderType }) => {
             setItems(response.data);
         } catch (e) {
             console.error(e);
+            setError(true)
         }
+    }
+    if (error) {
+        return <ErrorBanner message="에러가 발생했습니다." />
     }
     const ItemComponents = orderType === 'products' ? Products : null
     const optionItems = items.map((item) => (
