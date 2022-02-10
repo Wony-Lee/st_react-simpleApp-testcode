ed35992dea690e2eb5e047655f289b42cd42043d
Wony-Lee <a1_11_11@naver.com>
2022-02-10 22:48:06 +0900

    custom render

--- src/pages/OrderPage/tests/calculate.test.js
+++ src/pages/OrderPage/tests/calculate.test.js
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../../test-utils'
 import userEvent from '@testing-library/user-event';
 import { OrderContextProvider } from '../../../contexts/OrderContext'
 import Type from '../Type'
 
 test("update product's total when products change", async () => {
-    render(<Type orderType="products" />, { wrapper: OrderContextProvider })
+    render(<Type orderType="products" />)
 
     const productsTotal = screen.getByText("총 가격 : ", { exact: false });
     expect(productsTotal).toHaveTextContent("0")
